0ea7dc4757a9fc61b496b1bb9adbeb955d7d56ce
ThoernVE <viktor.e.thorn@hotmail.com>
2024-10-23 13:49:15 +0200

    removed comment in javascript-file

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/* Funktion för API-hämtning, mall
-function WeatherFunction() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat=15.61287516898284&lon=58.411616852847736&appid=df4c0154d893ba9c3ae1e611a27a169b")
-        .then((response) => response.json())  // Parse the JSON directly
-        .then((weatherData) => {
-            const weatherText = document.getElementById("weatherText"); // Assuming you have an element with this ID
-            translationSwitchCase(weatherData.weather[0].description)})
-
-} 
-*/
 function RaiderIOFunction(realm, charactername) {
     fetch(`https://raider.io/api/v1/characters/profile?region=eu&realm=${realm}&name=${charactername}&fields=mythic_plus_scores_by_season%3Acurrent`)
     .then((response) => response.json())
