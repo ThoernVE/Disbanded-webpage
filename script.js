b692689a7932e62757d93ac78feaaca55ef11482
ThoernVE <viktor.e.thorn@hotmail.com>
2024-10-23 13:28:31 +0200

    added a function in javascript to use Raider.IO API in order to get mythic+ score for current season using string interpolation. Therefore i can pass server and name into the function and use it for all raiders. Added the info to all raiders and made onclick-function to load API on demand. Got every character working except Rejzia

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function WeatherFunction() {
 
 } 
 */
+function RaiderIOFunction(realm, charactername) {
+    fetch(`https://raider.io/api/v1/characters/profile?region=eu&realm=${realm}&name=${charactername}&fields=mythic_plus_scores_by_season%3Acurrent`)
+    .then((response) => response.json())
+    .then((RaiderData) => {
+        const Score = document.getElementById(`${charactername}Score`);
+        Score.innerHTML = RaiderData.mythic_plus_scores_by_season[0].scores.all;
+
+    })
+}
