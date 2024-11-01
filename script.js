fac44a2d148b47297351a16f3b87eb9a87f53a1b
ThoernVE <viktor.e.thorn@hotmail.com>
2024-11-01 14:20:33 +0100

    refactored the API-functions to take variables instead and be able to used for other purposes by creating a class to store values.

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+const scoreObject = {
+    cutOffScore: 0,
+    mPlusScore: 0,
+};
+
 function RaiderIOFunction(realm, charactername) {
     fetch(`https://raider.io/api/v1/characters/profile?region=eu&realm=${realm}&name=${charactername}&fields=mythic_plus_scores_by_season%3Acurrent`)
         .then((response) => response.json())
         .then((RaiderData) => {
-            const Score = document.getElementById(`${charactername}Score`);
-            Score.innerHTML = RaiderData.mythic_plus_scores_by_season[0].scores.all;
+            scoreObject.mPlusScore = RaiderData.mythic_plus_scores_by_season[0].scores.all;}
 
-        })
+        )
 }
 
 function MPlusCutOff() {
     fetch("https://raider.io/api/v1/mythic-plus/season-cutoffs?season=season-tww-1&region=eu")
         .then((response) => response.json())
         .then((ScoreData) => {
-            const MScore = document.getElementById("mPlusCut");
-            MScore.innerHTML = ScoreData.cutoffs.p999.all.quantileMinValue;
+            scoreObject.cutOffScore = ScoreData.cutoffs.p999.all.quantileMinValue;
 
         })
 }
+
+function setInnerText(id, value) {
+    id.innerText = value;
+    console.log(`${id}`)
+}
+
